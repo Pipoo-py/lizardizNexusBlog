aad8218873df35bbfffb946704a7eb442350d33c
Alejandro Salazar Lizardiz <asalazarlizardiz@gmail.com>
2025-04-16 06:39:58 -0400

    First commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Lizardiz Nexus Blog';
+export const SITE_DESCRIPTION = 'Blog de Alejandro Salazar Lizardiz (Lizardiz Nexus), para compartir contenido sobre programación frontend.';
