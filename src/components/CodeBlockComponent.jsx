c538fb489f8fcbb131947541518b29c64592a709
Alejandro Salazar Lizardiz <asalazarlizardiz@gmail.com>
2025-04-17 17:12:40 -0400

    Fix codeblock issue in mobile version

--- src/components/CodeBlockComponent.jsx
+++ src/components/CodeBlockComponent.jsx
@@ -2,13 +2,16 @@ import { CodeBlock, atomOneLight, atomOneDark } from "react-code-blocks";
 import { theme } from '../stores/themeStore';
 
 export const CodeBlockComponent = ({ code, language, showLineNumbers, startingLineNumber }) => {
+  
   return (
-    <CodeBlock
-      text={code}
-      language={language}
-      showLineNumbers={showLineNumbers}
-      startingLineNumber={startingLineNumber}
-      theme={ theme == "light" ? atomOneLight : atomOneDark }
-    />
+   <div style={{ display: 'flex', justifyContent: 'center', width: '100%', margin: "42px 0" }}>
+      <CodeBlock
+        text={code}
+        language={language}
+        showLineNumbers={showLineNumbers}
+        startingLineNumber={startingLineNumber}
+        theme={theme === "light" ? atomOneLight : atomOneDark}
+      />
+    </div>
   );
 }
